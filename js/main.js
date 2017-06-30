c4ddde7ed0b673b918e1798dc589c0218060b03a
cindy <cindy.nicolay@laposte.net>
2017-06-30 11:37:46 +0200

    creation function, and  if desktop choice pierre

--- js/main.js
+++ js/main.js
@@ -1,100 +1,14 @@
-/*var choixUser = prompt("Choisissez-vous pierre, feuille, ou ciseaux ?");
-var choixOrdi = Math.random();
-
-var pointOrdi
-var pointUser
-
-if (choixOrdi < 0.34) {
-	choixOrdi = "pierre";
-}
-  else if(choixOrdi <= 0.67) {
-	choixOrdi = "feuille";
-}
-
-  else {
-	choixOrdi = "ciseaux";
-}
-
-
-
-if (choixOrdi === choixUser){
-  alert ("Egalité!");
-}
-
-else {
-
-  switch(choixUser) {
-
-
-    case "pierre":
-      console.log("l'ordinateur avait choisit : " + choixOrdi);
-
-    if (choixOrdi===ciseaux) {
-      alert ("gagné!");
-      pointUser ++
-    }
-
-    else {
-      alert ("perdu!");
-    pointOrdi ++
-    }
-
-  break;
-
-  case "ciseaux":
-
-  if (choixOrdi===feuille) {
-    alert ("gagné!");
-    pointUser ++
-}
-  else {
-    alert ("perdu");
-    pointOrdi ++
-
-}
-  break;
-
-  case "feuille":
-
-  if (choixOrdi===pierre) {
-    alert ("gagné!");
-  pointUser ++
-}
-  else {
-    alert ("perdu");
-    pointOrdi ++
-  }
-    break;
-
-    default:
-    alert ("n'importe quoi!");
-
-}
-}*/
-
-
-
-
-
-
-
 var pointUser = 0;
 var pointOrdi = 0;
 
+//On crée une boucle tant que les points n'ont pas atteint 3 points//
+while (pointUser !==3 && pointOrdi !==3) {
 
+  var choixUtilisateur = prompt("Choisissez-vous pierre, feuille, ou ciseaux ?");
+  var choixOrdi = Math.random(); //valeur aleatoire entre 0 et 1//
 
-
-
-
-
-  while (pointUser !==3 && pointOrdi !==3) {
-
-
-
-        var choixUtilisateur = prompt("Choisissez-vous pierre, feuille, ou ciseaux ?");
-        var choixOrdi = Math.random();
-
-        if (choixOrdi < 0.34) {
+//On traduit ce nombre aleatoire en un choix aleatoire entre pierre, papier, ciseaux//
+  if (choixOrdi < 0.34) {
         	choixOrdi = "pierre";
         }
           else if(choixOrdi <= 0.67) {
@@ -107,62 +21,30 @@ var pointOrdi = 0;
 
         alert("l'Ordinateur avait choisit : " + choixOrdi);
 
+//On crée une fonction avec les 2 choix qui retrournera le choix gagnant//
 function comparer (choixOrdi, choixUtilisateur) {
 
-if(choixOrdi === choixUtilisateur) {
+//Si les 2 choix sont les même//
+  if(choixOrdi === choixUtilisateur) {
          return("Egalité !");
     }
 
+//Si le choix de l'ordi est pierre//
   else if(choixOrdi === "pierre") {
 
   if(choixUtilisateur === "ciseaux") {
-        pointOrdi++;
+    pointOrdi++;
     return("pierre gagne!");
 
     }
   else {
     pointUser++;
-        return("feuille gagne !");
+    return("feuille gagne !");
 
     }
 }
 
-else if(choixOrdi==="feuille") {
 
-    if(choixUtilisateur==="pierre") {
-      pointOrdi++;
-        return (" feuille gagne !");
-
-    }
-    else {
-      pointUser++;
-        return ("ciseaux gagne !");
-
-    }
-
-}
-
-else if(choixOrdi==="ciseaux") {
-
-    if(choixUtilisateur==="pierre") {
-      pointUser++;
-        return(" pierre gagne !");
-
-    }
-    else {
-      pointOrdi++;
-        return("ciseaux gagne !");
-
-    }
-
-}
-
-
-
-    }
-
-alert ( comparer (choixOrdi, choixUtilisateur));
-    alert ("ordinateur: " + pointOrdi +"."+" "+ "vous: " + pointUser + ".");
 
 
 }
